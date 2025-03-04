1a09c69f41a741093bd4c573d36bae8a24deec0e
NeverEndingRadicalDude <110139042+NeverEndingRadicalDude@users.noreply.github.com>
2025-03-04 09:14:25 -0500

    update

--- gradle/src/main/java/com/render/utils/Transformation.java
+++ gradle/src/main/java/com/render/utils/Transformation.java
@@ -15,4 +15,16 @@ public class Transformation {
         return matrix;
     }
 
+    public static Matrix4f getViewMatrix(Camera camera) {
+        Vector3f pos = camera.getPosition();
+        Vector3f rot = camera.getRotation();
+        Matrix4f matrix = new Matrix4f();
+        matrix.identity();
+        matrix.rotate((float) Math.toRadians(rot.x), new Vector3f(1, 0, 0))
+            .rotate((float) Math.toRadians(rot.y), new Vector3f(0, 1, 0))
+            .rotate((float) Math.toRadians(rot.z), new Vector3f(0, 0, 1));
+        matrix.translate(-pos.x, -pos.y, -pos.z);
+        return matrix;
+    }
+
 }
